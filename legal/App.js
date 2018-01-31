3e68182703280513fe0fa84a3dd2e7cc535a7092
piggy2303 <nguyentuananh23031997@gmail.com>
2018-01-31 13:37:38 +0700

    test

--- legal/App.js
+++ legal/App.js
@@ -24,7 +24,7 @@ export default class App extends Component<{}> {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcomeeeeeeeeeeeee to React Native!fdgdfgdfgdf
+          version 1
         </Text>
         <Text style={styles.instructions}>
           To get started, edit App.jsdfgdfgfdgdfg
