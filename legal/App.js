6ed038c8a0a8252c9497ded565dc6a841f1dbd51
piggy2303 <nguyentuananh23031997@gmail.com>
2018-01-31 13:41:36 +0700

    test 2

--- legal/App.js
+++ legal/App.js
@@ -27,7 +27,7 @@ export default class App extends Component<{}> {
           version 1
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit App.jsdfgdfgfdgdfg
+          To get started, edit App.jsdfgdfgfdgdfgsdfsdf
         </Text>
         <Text style={styles.instructions}>
           {instructions}
